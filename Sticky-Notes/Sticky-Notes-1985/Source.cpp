59b0d64bcc7c9ca1bb11d2ce2ae338b2280cd36c
ibrahim123abusamrah <ibrahim.abusamrah123@gmail.com>
2019-07-17 12:43:00 +0300

    make fist case--> input data

---
+++ Sticky-Notes/Sticky-Notes-1985/Source.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <String>
+
+#include <fstream>
+using namespace std;
+
+int main() {
+
+    int choice;
+    
+        cout << endl
+             << "Welcome to the brand new “Sticky Notes”!.\n"
+             << "Here is the list of operation this program offers:.\n"
+             << " 1 - Add new user.\n"
+             << " 2 - Add new note.\n"
+             << " 2 - View notes for a specific user.\n"
+             << " 4 - Exit.\n"
+             << " Enter your choice and press return: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+//code to Add new user
+                cout << endl
+                     << "Welcome aboard new user!.\n"
+                     << "Please let me know your first name:.\n";
+				string first_name,last_name ;
+                cin >> first_name;
+                cout << endl
+                     << "Great "<<first_name<<" , now please enter your last name:";
+                cin >> last_name;
+                cout << endl
+                     << " Done!.\n"
+                     << " Nice to meet you .\n"<<first_name<<" "<<last_name
+                     << " .\n<Click Enter to return to main menu>.\n";
+//string filename=   ;   **first name+last name
+                cout << "Enter user name";
+                
+               
+
+                break;
+           
+    return 0;
+}
+}
